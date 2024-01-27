af99c6a183fff35f6e1e8026d6ef58bf603404c6
SirMoistPotatoViper <60393869+SirMoistPotatoViper@users.noreply.github.com>
2024-01-27 12:40:09 -0600

    Health bar working, shooting enemy working

--- Assets/Scripts/InGame/Bullet.cs
+++ Assets/Scripts/InGame/Bullet.cs
@@ -8,10 +8,15 @@ public class Bullet : MonoBehaviour
     public int damage = 40;
     public Rigidbody2D rb;
     public GameObject impactEffect;
+    public float impactEffectDuration = 0.5f;
+    //public LineRenderer lineRenderer;
+
+    private Transform spawnPoint;
     // Start is called before the first frame update
     void Start()
     {
         rb.velocity = transform.right * speed;
+        spawnPoint = transform;
     }
 
     private void OnTriggerEnter2D(Collider2D hitInfo)
@@ -22,7 +27,11 @@ public class Bullet : MonoBehaviour
             enemy.TakeDamage(damage);
         }
 
-        Instantiate(impactEffect, transform.position, transform.rotation);
+        GameObject impact = Instantiate(impactEffect, transform.position, transform.rotation);
+        Destroy(impact, impactEffectDuration);
+        //lineRenderer.SetPosition(0, spawnPoint.position);
+        //lineRenderer.SetPosition(1, spawnPoint.position + spawnPoint.right * 100);
+
 
         Destroy(gameObject);
     }
