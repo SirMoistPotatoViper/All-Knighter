3c6d0807498e4c8e549d6284ff70f1da9d643df7
Lraptor520 <144494848+Lraptor520@users.noreply.github.com>
2024-01-28 09:54:58 -0600

    a

--- Assets/Scripts/MotherHuntBehaviour.cs
+++ Assets/Scripts/MotherHuntBehaviour.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem.Android.LowLevel;
 
 public class MotherHuntBehaviour : MonoBehaviour
 {
@@ -8,17 +9,20 @@ public class MotherHuntBehaviour : MonoBehaviour
     public GameObject MotherHunt;
     public GameObject Player;
 
+    public Vector3 spawnPoint;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        spawnPoint = new Vector3(-3.484f, 0.434f, 0);
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "RealPlayer")
         {
-            
+            Player.transform.position = spawnPoint;
+
         }
     }
 
